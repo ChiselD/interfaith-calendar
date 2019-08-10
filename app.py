062a9b4687f87ab40b7810e7c86a1e09cf805d3d
Jinx <chisel.delahanty@gmail.com>
2019-08-10 15:28:53 +0200

    String next_sibling methods to handle weird DOM structure

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ else:
 	# print("Year found: " + year_url)
 
 
-### COLLECT HOLIDAYS ###
+### COLLECT ALL HOLIDAYS ###
 
 year_response = requests.get(year_url)
 year_html = year_response.text
@@ -69,29 +69,73 @@ current_month_h2 = find_month(h2s, month_regex)
 
 # WARNING:
 # Things get hacky ahead, because the HTML DOM structure is weird :/
-sib1 = current_month_h2.next_sibling
-sib2 = sib1.next_sibling
-holiday_days = sib2.contents[:-1]
+holiday_data_plus_definitions = current_month_h2.next_sibling.next_sibling
+holiday_data = holiday_data_plus_definitions.contents[:-1] # Here we strip the last item, "Definitions", which we don't want
 
-for holiday_day in holiday_days:
-	# Alternating items are blank, for some reason, so this line skips blanks:
-	if holiday_days.index(holiday_day) % 2 == 1:
-		day_number = holiday_day.contents[0].strip()
-		print("Day number: " + str(day_number))
-		if current_day == day_number:
-			print("There's a holiday today!")
-		# Check if day_number is a single day or multiple days
-		if str(day_number).isdigit() == False:
-			print("Multiple days!")
-		holidays_today_raw = holiday_day.contents[1] # type: bs4.element.Tag
+# Function to break down multi-day info and append all those days to a list
+def append_multiple_days(arr, data):
+	range_list = data.split("-")
+	for n in range(int(range_list[0]), int(range_list[1])+1):
+		arr.append(str(n))
+
+# Put all holiday days this month in a list
+holiday_days_this_month_with_duplicates = []
+# Each holiday_data_item contains 1. date, 2. holidays (w/ HTML), and optionally 3. blankness
+for holiday_data_item in holiday_data:
+	if holiday_data.index(holiday_data_item) % 2 == 1: # Alternating items are blank, for some reason, so this line skips blanks
+
+		print("Length of holiday_data_item after removing blanks: ")
+		print(len(holiday_data_item))
+		print("All remaining contents of holiday_data_item after removing blanks:")
+		for content in holiday_data_item.contents:
+			print("Content: ")
+			print(content)
 
 		holidays_today = []
-		for item in holidays_today_raw.contents:
-			if item != "\n":
-				holidays_today.append(item.get_text().strip())
-		print("RESULTS:")
-		for holiday in holidays_today:
-			print(holiday)
+
+		# Get day and append it to holidays_today
+		day_number = holiday_data_item.contents[0].strip()
+		if day_number.isdigit():
+			holiday_days_this_month_with_duplicates.append(day_number)
+		else:
+			multiday = True
+			append_multiple_days(holiday_days_this_month_with_duplicates, day_number)
+
+		# Get holiday info and append it to holidays_today
+		holidays_today.append(holiday_data_item.contents[1].get_text().strip()) # type: bs4.element.Tag
+
+# Remove duplicate day values while preserving order
+holiday_days_this_month = list(dict.fromkeys(holiday_days_this_month_with_duplicates))
+print("All holiday days this month:")
+print(holiday_days_this_month)
+
+# Function to list all the holidays that occur on a certain day
+# def list_todays_holidays(day):
+# 	pass
+
+# Check if there is a holiday today
+if current_day in holiday_days_this_month:
+	print("There's a holiday today!")
+	# list_todays_holidays(current_day)
+else:
+	print("There are no major holidays today.")
+
+
+holidays_today = [] # All plaintext holidays occurring today will go in this list
+for html_item in holidays_today_raw.contents:
+	if html_item != "\n":
+		holidays_today.append(html_item.get_text().strip())
+print("RESULTS:")
+for holiday in holidays_today:
+	print(holiday)
+print("\n")
+
+def report_todays_holidays(holidays):
+	print(f"Today is {current_month} {current_day}, {current_year}.")
+	for holiday in holidays:
+		print("Today is: " + holiday)
+
+report_todays_holidays(holidays_today)
 
 
 #################################################
