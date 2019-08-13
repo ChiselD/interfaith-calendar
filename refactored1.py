a2d722c388fc14d611c442a7796818601f3a60c4
Jinx <chisel.delahanty@gmail.com>
2019-08-13 19:47:27 +0200

    Refactor: Create function to get URL of page for chosen year.

--- refactored1.py
+++ refactored1.py
@@ -37,7 +37,24 @@ def sanitize_input(date_as_tuple):
 	return (sanitized_year, sanitized_month, sanitized_day)
 
 def get_year_page(year):
-	pass
+	soup = BeautifulSoup(requests.get("http://www.interfaith-calendar.org/index.htm").text, "html.parser")
+	links = soup.findAll("a")
+	hrefs = []
+	for link in links:
+		hrefs.append(link.get("href"))
+
+	# Get possible link format to year page
+	year_option_1 = str(year) + ".htm"
+	year_option_2 = str(year) + ".html"
+
+	# Get link directly to current year page
+	result = ""
+	if year_option_1 in hrefs:
+		result = year_option_1
+	elif year_option_2 in hrefs:
+		result = year_option_2
+
+	return "http://www.interfaith-calendar.org/" + result
 
 def get_month_data(month):
 	pass
@@ -50,15 +67,17 @@ def report_todays_holidays(today):
 
 def check_calendar(year=get_current_date()['year'], month=get_current_date()['month'], day=get_current_date()['day']):
 	year, month, day = sanitize_input((year, month, day))
-	print("current['year'] is: " + year)
-	print("current['month'] is: " + month)
-	print("current['day'] is: " + day)
+	print(f"Today is {month} {day}, {year}.")
+
 	year_page = get_year_page(year)
+	print("year_page successfully found:")
+	print(year_page)
+
 	month_data = get_month_data(month)
 	cleaned_month_data = clean_month_data(month_data)
 	print(report_todays_holidays(cleaned_month_data))
 
-check_calendar()
-# check_calendar(2018,"dec",31)
+# check_calendar()
+check_calendar(2019,"december",31)
 
 ###
