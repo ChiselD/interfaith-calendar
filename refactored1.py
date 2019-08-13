8dca54e5c8a62e9cf55f3958995cddcecd41ab7f
Jinx <chisel.delahanty@gmail.com>
2019-08-13 13:49:24 +0200

    Refactor: Set up skeletons for all remaining top-level functions.

--- refactored1.py
+++ refactored1.py
@@ -8,18 +8,32 @@ def get_current_date():
 	today = date.today()
 	return {
 		"year": today.strftime("%Y"),
-		"month": today.strftime("%B"),
-		# "month": "February",
-		"day": today.strftime("%d").lstrip("0")
-		# "day": "1"
+		# "month": today.strftime("%B"),
+		"month": "February",
+		# "day": today.strftime("%d").lstrip("0")
+		"day": "1"
 	}
+
+def get_year_page(year):
+	pass
+
+def get_month_data(month):
+	pass
+
+def clean_month_data(month_data):
+	pass
+
+def report_todays_holidays(today):
+	pass
 
 def check_calendar():
 	current = get_current_date()
 	print("current['year'] is: " + current['year'])
 	print("current['month'] is: " + current['month'])
 	print("current['day'] is: " + current['day'])
-	# year_page = get_year_page()
-	# get_current_month_data()
+	year_page = get_year_page(current['year'])
+	month_data = get_month_data(current['month'])
+	cleaned_month_data = clean_month_data(month_data)
+	print(report_todays_holidays(cleaned_month_data))
 
 check_calendar()
